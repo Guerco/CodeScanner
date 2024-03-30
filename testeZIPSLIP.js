678b2cccf1b233627228db1b613f07d9ebfccab4
Guerco <gabrielszgrco@gmail.com>
2024-03-30 20:18:33 -0300

    Update testeZIPSLIP.js

--- testeZIPSLIP.js
+++ testeZIPSLIP.js
@@ -1,10 +1,1 @@
-const fs = require('fs');
-const AdmZip = require('adm-zip'); // Biblioteca para manipular arquivos zip
-
-function extrairArquivoZip(arquivoZip, pastaDestino) {
-    const zip = new AdmZip(arquivoZip);
-    zip.extractAllTo(pastaDestino, true);
-}
-
-// Exemplo de uso
-extrairArquivoZip('arquivo.zip', 'pasta_destino');
+var srcpath = path.resolve(cwd, header.linkname)
